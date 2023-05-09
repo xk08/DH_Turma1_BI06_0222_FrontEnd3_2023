9144c5f5ef163065eedf9c8cbe70d22cb1a17655
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-05-09 20:22:40 -0300

    Aula 2, template inicial p/ aula

---
+++ Aula2--FerramentasEPrimeiroProjeto/Aula2--VanillaToReact/index.js
@@ -0,0 +1,12 @@
+/// Buscando o conteiner root do HTML onde o React irá atuar
+const div = document.getElementById("div");
+
+/// Função createRoot()
+
+/// Novo elemento React
+
+
+/// Função render()
+
+
+/// Component com JSX
