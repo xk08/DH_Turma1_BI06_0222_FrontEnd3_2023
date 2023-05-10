45edcfdea8d95bb4f5536502648b714fd610627f
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-05-09 22:28:20 -0300

    Aula 2, códigos feitos em aula + Mesa de trabalho

--- Aula2--FerramentasEPrimeiroProjeto/Aula2--VanillaToReact/index.js
+++ Aula2--FerramentasEPrimeiroProjeto/Aula2--VanillaToReact/index.js
@@ -1,12 +1,41 @@
 /// Buscando o conteiner root do HTML onde o React irá atuar
 const div = document.getElementById("div");
+const lista = document.getElementById("lista");
 
 /// Função createRoot()
+const divRoot = ReactDOM.createRoot(div);
+const listaRoot = ReactDOM.createRoot(lista);
 
-/// Novo elemento React
+/// Criando novo elemento React (sem JSX)
+const element = React.createElement("h1", {}, "Front end III");
+
+/// Componente React do tipo funcional (com JSX)
+function ElementJSX() {
+    return (
+        <h1>Front End III</h1>
+    );
+}
+
+/// Componente React do tipo funcional
+function ListaAlunos() {
+    return (
+        <div>
+            <h1>Marcos</h1>
+            <h1>Pedro</h1>
+            <h1>Ana</h1>
+        </div>
+    );
+}
+
+/// Renderizando o elemento 
+//divRoot.render(element);
+divRoot.render(<ElementJSX />);
+listaRoot.render(<ListaAlunos />)
 
 
-/// Função render()
+
+
+
 
 
 /// Component com JSX
---
+++ Aula2--FerramentasEPrimeiroProjeto/aula2_primeiro_projeto_cra/src/App.js
@@ -0,0 +1,6 @@
+/// Componente funcional
+function App() {
+  return <h1>Olá mundo!</h1> /* com JSX */
+}
+
+export default App;
