f9f9ff148a2272f69bc9d08e56aa29f2a6cac784
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-05-24 19:19:36 -0300

    Aula 9: organização dos arquivos + template p/ mesa de trabalho

---
+++ Aula9--RevisaoEPratica3/MesaDeTrabalho_template/src/App.jsx
@@ -0,0 +1,61 @@
+import "./styles.css";
+
+function App() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Média de Alunos por Disciplina</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="container_input">
+          <select>
+            <option selected disabled>
+              Selecione uma disciplina
+            </option>
+            <option value="database">Banco de Dados</option>
+            <option value="backend">Desenvolvimento Backend</option>
+            <option value="frontend">Desenvolvimento Frontend</option>
+            <option value="devops">Devops</option>
+          </select>
+          <input />
+        </div>
+        <input type="submit" value="Salvar" />
+      </form>
+
+      <div className="container">
+        <table border="1" className="line_title">
+          <thead>
+            <tr>
+              <th>Disciplina</th>
+              <th>Quantidade de Alunos</th>
+              <th>Média das notas dos alunos</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Banco de Dados</td>
+              <td>0</td>
+              <td>0</td>
+            </tr>
+
+            <tr>
+              <td>Desenvolvimento Frontend</td>
+              <td>0</td>
+              <td>0</td>
+            </tr>
+
+            <tr>
+              <td>Desenolvimento Backend</td>
+              <td>0</td>
+              <td>0</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
