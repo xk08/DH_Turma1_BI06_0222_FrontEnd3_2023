47d9f8a798065b45990de72b8affbf39df7bd1de
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-06-04 23:46:20 -0300

    Aula 13 - Ciclo de vida: Códigos base p/ aula

---
+++ Aula13--CicloDeVida/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h2>Ciclo de Vida (Hook: useEffect())</h2>
+    </>
+  )
+}
+
+export default App
---
+++ Aula13--CicloDeVida/src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  /* TODO: Lembrar de retirar o Strict-Mode para essa aula! */
+  <App />
+)
